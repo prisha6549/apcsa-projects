3f3a0b9e7633e1f125b79bb2ea0b24a5de779cb8
prisha6549 <nprisha03@gmail.com>
2025-09-04 20:48:34 +0000

    h

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -2,7 +2,9 @@ package robot;
 import kareltherobot.*;
 
 public class Roomba implements Directions {
+    {
     Robot rom = new Robot(15,2,South,9);
     rom.move(); 
-    rom.putBeeeper();
+    rom.putBeeper();
+    }
 }
